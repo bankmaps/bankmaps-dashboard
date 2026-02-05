99c7ca1c5ae23e67cfab588950df4ba7c1af9efb
bankmaps <administrator@bankmaps.com>
2026-02-05 12:00:01 -0500

    Update page.jsx

--- app/create-account/page.jsx
+++ app/create-account/page.jsx
@@ -7,6 +7,8 @@ import Select from 'react-select';
 import { useRouter } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
 
 const similarity = (a, b) => {
   a = a.toLowerCase();
