89b98541ea1c53c4267e77e76f6fb89a80de2cae
bankmaps <administrator@bankmaps.com>
2026-02-09 10:18:23 -0500

    Update page.tsx

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import ManageProfile from "./features/ManageProfile";
+
 
 export default function UsersPage() {
   const [openSection, setOpenSection] = useState<string | null>("Dashboard");
