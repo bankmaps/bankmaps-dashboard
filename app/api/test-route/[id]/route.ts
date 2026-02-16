7ba69a2bdaf5e91c4f98f54ff4a601da9d48efa1
bankmaps <administrator@bankmaps.com>
2026-02-16 10:22:31 -0500

    Delete app/api/test-route/[id]/route.ts

--- app/api/test-route/[id]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(
-  req: Request,
-  context: { params: Promise<{ id: string }> }
-) {
-  const params = await context.params;
-  return NextResponse.json({ message: 'It works!', id: params.id });
-}
